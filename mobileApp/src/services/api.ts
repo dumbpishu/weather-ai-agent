a917c1fa77a90c4b934dba2f2449d743c04972ee
Priyanshu Singh <priyanshuparashar08@gmail.com>
2026-02-22 21:04:13 +0530

    fix ui and make them working

--- mobileApp/src/services/api.ts
+++ mobileApp/src/services/api.ts
@@ -1,18 +1,10 @@
-export const sendAudioToBackend = async (uri: string) => {
-    const formData = new FormData();
-
-    formData.append("audio", {
-        uri,
-        name: "voice.m4a",
-        type: "audio/m4a"
-    } as any);
-
+export const sendRequestToBackend = async (text: string) => {
     const res = await fetch("https://weather-ai-agent-iw5p.onrender.com/agent", {
         method: "POST",
-        body: formData,
         headers: {
-            "Content-Type": "multipart/form-data"
-        }
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ q: text })
     });
 
     return await res.json();
